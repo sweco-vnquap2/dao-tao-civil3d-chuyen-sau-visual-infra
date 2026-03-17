c57bbf3e6ab4c2ea89318da5dadc12a3ac8190bc
sweco-vnquap2 <quangpt.tw@gmail.com>
2026-03-17 23:20:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const goToZalo = () => {
+    window.location.href = 'https://zalo.me/0937444368';
+};
+
+document.getElementById('btnHeader').addEventListener('click', goToZalo);
+document.getElementById('btnContact').addEventListener('click', goToZalo);
